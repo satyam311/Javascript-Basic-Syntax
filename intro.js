1e7447778194c4ed9bd3abfa034e2ab33b72ce24
satyam <satyam987321@gmail.com>
2021-07-19 00:04:51 +0530

    Introduction

---
+++ intro.js
@@ -0,0 +1,42 @@
+// console.log("hello world");
+// // declaring the variable
+// let a;
+// // By default value in a variable is undefined 
+
+// console.log(a);
+
+// a = 10;
+// a = 10.1;
+// a = "Hello I am a String "
+// a = "hello it me a String"
+
+// console.log("Variable contains : ", a)
+//     // variable data type : Primitive Datatype : numser, String , boolean , null, symbols
+// Non Primituve : arrays , objects, function 
+
+// a = null;
+// a = true;
+
+// console.log(a);
+
+// javascript - syntaxistically is equal to java
+// let number = 10;
+// for (let i = 1; i <= number; i++) {
+//     console.log("number is : ", i);
+// }
+
+// let number = 23;
+// let flag = true;
+
+// for (let i = 2; i < number; i++) {
+//     if (number % i == 0) {
+//         flag = false;
+//         break;
+//     }
+// }
+
+// if (flag == true) {
+//     console.log("is Prime");
+// } else {
+//     console.log("is not Prime");
+// }
