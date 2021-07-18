2e4fef1f07512dcc25ffb7c196fa00e83f9b1799
satyam <satyam987321@gmail.com>
2021-07-19 00:04:28 +0530

    About function

---
+++ functions.js
@@ -0,0 +1,15 @@
+// function sayHi() {
+//     console.log("hello mishra G");
+// }
+
+// sayHi();
+
+function run(param) {
+    let rval = Math.random() > 0.5 ? true : "Not my type";
+    return rval;
+
+}
+
+let result = run(10);
+
+console.log(result);
